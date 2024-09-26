c6c85b527d9aefc0f3f57533bf9ed8b05a110943
isTheo <matteohg07@gmail.com>
2024-09-26 22:39:26 +0200

    Initial Commit

---
+++ Project28-SecretSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project28-SecretSwift
+//
+//  Created by Matteo Orru on 26/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+    
+    @IBOutlet var secret: UITextView!
+    
+    
+    
+}
+
